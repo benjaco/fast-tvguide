e95fe98cf9585d8e72123b8444ad04cad22f49e7
Benjamin Schultz Larsen <benjaco1996+edu@gmail.com>
2017-05-03 11:10:55 +0200

    get available channels add to the api

---
+++ server/get_channels.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Benjamin
+ * Date: 03-05-2017
+ * Time: 11:05
+ */
+
+
+
+echo file_get_contents(__DIR__ . "/data/channels/dk_channel_names_manuel.json");
