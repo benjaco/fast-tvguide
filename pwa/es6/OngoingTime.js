3d390f7e6db41dc5102939fd2b6eaa7aa08f7f28
Benjamin Schultz Larsen <benjaco1996+edu@gmail.com>
2017-05-19 00:42:28 +0200

    plain tv guide - update past state add meta html tag

--- pwa/es6/OngoingTime.js
+++ pwa/es6/OngoingTime.js
@@ -3,13 +3,14 @@
  */
 
 
-export default class OngoingTime{
+export default class OngoingTime {
 
     constructor(app) {
         this._nowEl = document.getElementsByClassName("now")[0];
         this._app = app;
 
         this.update = this.update.bind(this);
+        this.updateElements = this.updateElements.bind(this);
         this.update();
 
         setTimeout(_ =>  {
@@ -26,6 +27,21 @@ export default class OngoingTime{
 
         this._nowEl.style.left = (this.time - this._app.anchor) / 60 / 60 * this._app.responsive.timeLength + "px";
 
+        this.updateElements()
 
+    }
+
+    updateElements() {
+        const channelsEl = document.getElementsByClassName("programms");
+        Array.from(channelsEl).forEach(channelEl => {
+
+            let nextProgram = channelEl.querySelector(".program-wrapper:not(.program-in-the-past)");
+            if (nextProgram) {
+                let endsAt = parseInt(nextProgram.getAttribute("data-end"));
+                if (endsAt < this.time) {
+                    nextProgram.classList.add("program-in-the-past");
+                }
+            }
+        });
     }
 }
