72c3652782187ad51979a00315e90867f3b21728
Benjamin Schultz Larsen <benjaco1996+edu@gmail.com>
2017-05-25 23:25:59 +0200

    minify

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,13 @@
+// npm-run uglifycss pwa/style.css > pwa/style.min.css & npm-run webpack & npm-run html-minifier pwa/dev-index.html -o pwa/index.html --collapse-whitespace --remove-comments
+const BabiliPlugin = require("babili-webpack-plugin");
 
 module.exports = {
     entry: './pwa/es6/App.js',
     output: {
         filename: './pwa/app.js'
     },
-    devtool: 'source-map'
+    devtool: 'source-map',
+    plugins: [
+        new BabiliPlugin(),
+    ]
 };
